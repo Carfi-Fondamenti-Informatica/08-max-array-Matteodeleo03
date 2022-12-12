22de5ffe3f28b1b087af30bb839ec0c408a013a7
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-12-12 01:40:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+
+int main() {
+    int a;
+
+    cin >> a;
+    float array[a];
+    for (int i = 0; i < a; i++) {
+
+
+        cin >> array[i];
+    }
+
+
+    cout << max(array, a);
+
+    return 0;
+
 }
